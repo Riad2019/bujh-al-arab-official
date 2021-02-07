fbb6f74bafde0631266841c5f8a67132ddda24cb
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2021-02-07 18:22:57 +0600

    processinng

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_3Z0bnOVH5EcP5flyzscPrI154lwGkVI",
+    authDomain: "burj-al-arab-official.firebaseapp.com",
+    projectId: "burj-al-arab-official",
+    storageBucket: "burj-al-arab-official.appspot.com",
+    messagingSenderId: "663007110841",
+    appId: "1:663007110841:web:0d653049602c2808129d68"
+  };
+  export default firebaseConfig;
